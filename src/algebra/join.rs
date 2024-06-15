5192833c5f61cceb822b2713bc47489a4e4a56cf
datomo <dave.lengw@gmail.com>
2024-06-15 19:22:30 +0200

    added tests for join

--- src/algebra/join.rs
+++ src/algebra/join.rs
@@ -9,16 +9,28 @@ pub trait Join: Algebra {
     fn right(&self) -> &dyn Algebra;
 }
 
-pub struct TrainJoin<'a> {
+pub struct TrainJoin<'a, Hash>
+where
+    Hash: PartialEq,
+{
     left: &'a dyn Algebra,
     right: &'a dyn Algebra,
-    left_hash: Arc<Box<dyn Fn(&Value) -> &str>>,
-    right_hash: Arc<Box<dyn Fn(&Value) -> &str>>,
+    left_hash: Arc<Box<dyn Fn(&Value) -> &Hash>>,
+    right_hash: Arc<Box<dyn Fn(&Value) -> &Hash>>,
     out: Arc<Box<dyn Fn(Value, Value) -> Value>>,
 }
 
-impl TrainJoin {
-    pub(crate) fn new(left: &dyn Algebra, right: &dyn Algebra, left_hash: Box<dyn Fn(&Value) -> &str>, right_hash: Box<dyn Fn(&Value) -> &str>, out: Box<dyn Fn(Value, Value) -> Value>) -> Self {
+impl<'a, H> TrainJoin<'a, H>
+where
+    H: PartialEq,
+{
+    pub(crate) fn new(
+        left: &'a dyn Algebra,
+        right: &'a dyn Algebra,
+        left_hash: Box<dyn Fn(&Value) -> &H>,
+        right_hash: Box<dyn Fn(&Value) -> &H>,
+        out: Box<dyn Fn(Value, Value) -> Value>,
+    ) -> Self {
         TrainJoin {
             left,
             right,
@@ -29,7 +41,7 @@ impl TrainJoin {
     }
 }
 
-impl Algebra for TrainJoin<'_> {
+impl<H: PartialEq + 'static> Algebra for TrainJoin<'_, H> {
     fn get_handler(&self) -> Box<dyn Fn() -> Train> {
         let left_hash = Arc::clone(&self.left_hash);
         let right_hash = Arc::clone(&self.right_hash);
@@ -43,12 +55,12 @@ impl Algebra for TrainJoin<'_> {
                 let mut values = vec![];
                 let left = left();
                 let right = right();
-                let right_hashes: Vec<(&str, Value)> = right.values.iter().map(|value: &Value| (right_hash(value), value.clone())).collect();
-                for (i, l_value) in left.values.iter().enumerate() {
-                    let l_hash = left_hash(l_value);
-                    for (j, r_hash) in right_hashes.iter().enumerate() {
-                        if l_hash == (*r_hash).0 {
-                            values.push(out(l_value.clone(), (*r_hash).1.clone()));
+                let right_hashes: Vec<(&H, Value)> = right.values.iter().map(|value: &Value| (right_hash(value), value.clone())).collect();
+                for l_value in left.values {
+                    let l_hash = left_hash(&l_value);
+                    for (r_hash, r_val) in &right_hashes {
+                        if l_hash == *r_hash {
+                            values.push(out(l_value.clone(), r_val.clone()));
                         }
                     }
                 }
@@ -58,7 +70,7 @@ impl Algebra for TrainJoin<'_> {
     }
 }
 
-impl Join for TrainJoin<'_> {
+impl<H: PartialEq + 'static> Join for TrainJoin<'_, H> {
     fn left(&self) -> &dyn Algebra {
         self.left
     }
@@ -70,18 +82,39 @@ impl Join for TrainJoin<'_> {
 
 #[cfg(test)]
 mod test {
-    use std::hash::Hash;
-
+    use crate::algebra::algebra::Algebra;
     use crate::algebra::join::TrainJoin;
     use crate::algebra::scan::TrainScan;
     use crate::processing::Train;
     use crate::value::Value;
 
     #[test]
-    fn simple() {
+    fn one_match() {
         let left = TrainScan::new(Train::new(vec![3.into(), 5.5.into()]));
-        let right = TrainScan::new(Train::new(vec!["test".into(), 5.5.into()]));
+        let right = TrainScan::new(Train::new(vec![5.5.into(), "test".into()]));
 
-        let join = TrainJoin::new(&left, &right, |val: Value| val.hash());
+        let join = TrainJoin::new(&left, &right, Box::new(|val: &Value| val), Box::new(|val: &Value| val), Box::new(|left: Value, right: Value| {
+            vec![left.into(), right.into()].into()
+        }));
+
+        let handle = join.get_handler();
+        let res = handle();
+        assert_eq!(res.values, vec![vec![5.5.into(), 5.5.into()].into()]);
+        assert_ne!(res.values, vec![vec![].into()]);
+    }
+
+    #[test]
+    fn multi_match() {
+        let left = TrainScan::new(Train::new(vec![3.into(), 5.5.into()]));
+        let right = TrainScan::new(Train::new(vec![5.5.into(), 5.5.into()]));
+
+        let join = TrainJoin::new(&left, &right, Box::new(|val: &Value| val), Box::new(|val: &Value| val), Box::new(|left: Value, right: Value| {
+            vec![left.into(), right.into()].into()
+        }));
+
+        let handle = join.get_handler();
+        let res = handle();
+        assert_eq!(res.values, vec![vec![5.5.into(), 5.5.into()].into(), vec![5.5.into(), 5.5.into()].into()]);
+        assert_ne!(res.values, vec![vec![].into()]);
     }
 }
