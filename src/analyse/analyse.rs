738cff59f7826c9255910295311db5d8475745fa
datomo <dave.lengw@gmail.com>
2024-10-18 16:09:10 +0200

    added analysis logic

--- src/analyse/analyse.rs
+++ src/analyse/analyse.rs
@@ -1,5 +1,5 @@
-use crate::analyse::summery::{Status, Summery};
 use crate::analyse::summery::StatusTypes::Islands;
+use crate::analyse::summery::{Status, Summery};
 use crate::processing::Plan;
 
 pub struct Analyser<'plan>{
@@ -20,20 +20,21 @@ impl<'plan> Analyser<'plan> {
         // islands?
         //stations
         self.plan.stations.values().for_each(|s| {
-            if !self.plan.lines.iter().any(|(_num, l)| l.contains(*s.id)) && !self.plan.stations_to_in_outs.contains_key(&s.id){
-                summery.add_stop_status(s.id, Status::WARNING(Islands, format!("Station {} is not connected to anything.", *s.id)));
+            if !self.plan.lines.iter().any(|(_num, l)| l.contains(&s.id)) && !self.plan.stations_to_in_outs.contains_key(&s.id){
+                todo!();
+                summery.add_stop_status(s.id, Status::WARNING(Islands, format!("Station {} is not connected to anything.", s.id)));
             }
         });
 
         self.plan.sources.values().for_each(|s| {
-            if !self.plan.stations_to_in_outs.iter().any(|(_, in_outs)|{ in_outs.contains(*s.get_id()) }){
-                summery.add_in_status(s.get_id(), Status::WARNING(Islands, format!("Source {} is not connected to anything.", *s.get_id())));
+            if !self.plan.stations_to_in_outs.iter().any(|(_, in_outs)|{ in_outs.contains(&s.get_id()) }){
+                summery.add_in_status(s.get_id(), Status::WARNING(Islands, format!("Source {} is not connected to anything.", s.get_id())));
             }
         });
 
         self.plan.destinations.values().for_each(|s| {
-            if !self.plan.stations_to_in_outs.iter().any(|(_, in_outs)|{ in_outs.contains(*s.get_id()) }){
-                summery.add_out_status(s.get_id(), Status::WARNING(Islands, format!("Destination {} is not connected to anything.", *s.get_id())));
+            if !self.plan.stations_to_in_outs.iter().any(|(_, in_outs)|{ in_outs.contains(&s.get_id()) }){
+                summery.add_out_status(s.get_id(), Status::WARNING(Islands, format!("Destination {} is not connected to anything.", s.get_id())));
             }
         });
 
@@ -48,13 +49,25 @@ pub fn analyse(plan: &Plan) -> Result<Summery, String> {
 
 #[cfg(test)]
 mod tests {
-    use crate::analyse;
     use crate::analyse::analyse::analyse;
+    use crate::analyse::summery::{Status, StatusTypes};
     use crate::processing::Plan;
 
     #[test]
     fn test_islands(){
-        let plan = Plan::parse("1").unwrap();
+        let plan = Plan::parse("In\nDummy{}").unwrap();
         let analyse = analyse(&plan);
+        assert!(analyse.is_ok());
+        let analyse = analyse.unwrap();
+        assert!(!analyse.is_ok());
+        let warnings = analyse.get_warnings();
+
+        assert_eq!(warnings.len(), 1);
+        match warnings[0].clone() {
+            Status::WARNING(st, _) => {
+                assert_eq!(StatusTypes::Islands, st);
+            }
+            _ => panic!("Wrong type of status")
+        }
     }
 }
--- src/analyse/summery.rs
+++ src/analyse/summery.rs
@@ -5,11 +5,10 @@ use std::process::id;
 pub struct Summery{
     status: Status,
     stops: HashMap<i64, Status>,
-    ins: HashMap<i64, String>,
-    outs: HashMap<i64, String>,
+    ins: HashMap<i64, Status>,
+    outs: HashMap<i64, Status>,
     complex: Vec<(Status, Vec<i64>)>,
 }
-
 
 impl Summery{
     pub fn new() -> Self{
@@ -18,6 +17,37 @@ impl Summery{
 
     pub fn is_ok(&self) -> bool{
         self.status.is_ok() && self.stops.iter().all(|(_, stop)| stop.is_ok()) && self.complex.iter().all(|(status, _lines)| status.is_ok())
+    }
+
+    pub(crate) fn get_warnings(&self) -> Vec<Status> {
+        let mut warnings = Vec::new();
+        if !self.is_ok(){
+            warnings.push(self.status.clone());
+        }
+        self.ins.iter().for_each(|(_, ins)|{
+           if ins.is_ok() {
+               warnings.push(ins.clone());
+           }
+        });
+
+        self.outs.iter().for_each(|(_, outs)|{
+            if outs.is_ok() {
+                warnings.push(outs.clone());
+            }
+        });
+
+        self.stops.values().for_each(|stop|{
+            if stop.is_ok() {
+                warnings.push(stop.clone());
+            }
+        });
+
+        self.complex.iter().for_each(|(status, lines)| {
+            if !status.is_ok(){
+                warnings.push(status.clone());
+            }
+        });
+        warnings
     }
 
     pub fn set_status(&mut self, status: Status){
@@ -29,11 +59,11 @@ impl Summery{
     }
 
     pub fn add_in_status(&mut self, id: i64, status: Status){
-        self.ins.insert(id, status.to_string());
+        self.ins.insert(id, status);
     }
 
     pub fn add_out_status(&mut self, id: i64, status: Status){
-        self.outs.insert(id, status.to_string());
+        self.outs.insert(id, status);
     }
 
     pub fn add_complex_status(&mut self, status: Status, ids: Vec<i64>){
@@ -41,7 +71,7 @@ impl Summery{
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub enum Status {
     #[default]
     OK,
@@ -58,6 +88,7 @@ impl Status {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum StatusTypes{
     Islands
 }
